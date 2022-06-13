8ea7998b3437291d01726a7b664945e418f41e6c
Aleksei D <79357673+motostrelok1@users.noreply.github.com>
2022-06-13 21:32:24 +0300

    Update Program.cs
    добавлен код задачи 2

--- lesson 1/Task 2/Program.cs
+++ lesson 1/Task 2/Program.cs
@@ -1,2 +1,17 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+// Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
+
+Console.WriteLine("Привет, эта программа находит наибольшее из трех чисел");
+
+int[] arr = new int[3];
+
+for (int i = 0; i <3; i++)
+{
+    Console.WriteLine(" Введи число " + (i+1));
+    arr[i] =  Convert.ToInt32(Console.ReadLine());
+}
+
+for (int i = 0; i <3; i++)
+{
+    Console.Write(" "+arr[i]);
+}
+Console.WriteLine(" max = "+arr.Max());
