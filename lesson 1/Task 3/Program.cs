c95f1b2c3c1a7d166c1e766c509bec9b0efea6f5
Alexei Dubovskoi <motostrelok1@yandex.ru>
2022-06-13 21:41:13 +0300

    добавлен код третьей задачи

--- lesson 1/Task 3/Program.cs
+++ lesson 1/Task 3/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Напишите программу, которая на вход принимает число и выдаёт, 
+//является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Привет, это программа для определения четности числа");
+
+Console.WriteLine(" Введи число ");
+int number =  Convert.ToInt32(Console.ReadLine());
+if (number % 2 == 0)
+{
+    Console.WriteLine("введенное число четное");
+}else {
+    Console.WriteLine("введенное число не четное");
+}
