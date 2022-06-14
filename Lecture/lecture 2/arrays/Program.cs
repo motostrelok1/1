d28a140f06818607289719533e80bb4db1e49407
Alexei Dubovskoi <motostrelok1@yandex.ru>
2022-06-14 12:57:09 +0300

    new folders

--- Lecture/lecture 2/arrays/Program.cs
+++ Lecture/lecture 2/arrays/Program.cs
@@ -1,2 +1,27 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+Console.WriteLine("arrays example");
+
+void FillArray(int[] collection){
+    int lenth = collection.Length;
+    int index = 0;
+    while (index<lenth){
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+
+}
+
+void PrintArray(int[] col){
+    int count = col.Length;
+    int position = 0;
+    while (position<count)
+    {
+        Console.Write(" " + col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
--- lesson 2/Task 1/Program.cs
+++ lesson 2/Task 1/Program.cs
@@ -1,18 +1,49 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("привет эта программ принимает на вход трехзначное число");
 
-void Rand(){
+int Rand(out int randomNumber){
     int randomNumber = new Random().Next(100,1000);
 //  Console.WriteLine(randomNumber+" "+(randomNumber%100/10));
 //  int result = randomNumber % 10;
 //  Console.WriteLine("   ");
-//return randomNumber;
+return randomNumber;
 }
 
-int[] arr = new int[3];
+//int[] arr = new int[3];
 // for (int i = 0; i < 3; i++)
 // {
 //    arr[i]=Rand.randomNumber;
 //    Console.WriteLine(arr[i]);
 // }
 Console.Writeline(Rand.randomNumber());
+
+
+
+
+// See https://aka.ms/new-console-template for more information
+Console.WriteLine("arrays example");
+
+void FillArray(int[] collection){
+    int lenth = collection.Length;
+    int index = 0;
+    while (index<lenth){
+        collection[index] = new Random().Next(1,10);
+        index++;
+    }
+
+}
+
+void PrintArray(int[] col){
+    int count = col.Length;
+    int position = 0;
+    while (position<count)
+    {
+        Console.Write(" " + col[position]);
+        position++;
+    }
+}
+
+int[] array = new int[10];
+
+FillArray(array);
+PrintArray(array);
