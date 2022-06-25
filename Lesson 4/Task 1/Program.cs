f8a40007fb4e626437f9fd1b0723da1fe5224bbe
Alexei Dubovskoi <motostrelok1@yandex.ru>
2022-06-25 17:56:38 +0300

    добавлены задачи дз

--- Lesson 4/Task 1/Program.cs
+++ Lesson 4/Task 1/Program.cs
@@ -2,12 +2,15 @@
 //в натуральную степень B.
 // 3, 5 -> 243 (3⁵)
 // 2, 4 -> 16
+
 Console.Clear();
 System.Console.WriteLine();
+
 // ввод данных
 Console.WriteLine(" Введите целое число А ");
 var num = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine(" Введите целое число В ");
 var pow = Convert.ToDouble(Console.ReadLine());
+
 // вывод в консоль
 Console.WriteLine(" Число " + num + " возведенное в степень " + pow + " = " + Math.Pow(num,pow));
